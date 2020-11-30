ccd16782f4ba83ee9a1e6109b5b45267627663bf
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-11-30 16:41:26 -0500

    Updated repo for Bridges2

---
+++ 2.1.5/modulefile.lua
@@ -0,0 +1,52 @@
+--
+-- ABySS 2.1.5 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: ABySS is a de novo sequence assembler intended for short paired-end reads and large genomes."
+
+whatis("Name: ABySS")
+whatis("Version: 2.1.5")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: ABySS is a de novo sequence assembler intended for short paired-end reads and large genomes.")
+
+help([[
+ABySS 2.1.5
+-----------
+
+Description
+-----------
+ABySS is a de novo sequence assembler intended for short paired-end reads and large genomes.
+
+To load the module type
+
+> module load abyss/2.1.5
+
+To unload the module type
+
+> module unload abyss/2.1.5
+
+Documentation
+-------------
+https://www.bcgsc.ca/resources/software/abyss
+
+For help, type
+
+> abyss-fixmate --help
+> abyss-pe --help
+
+Repository
+----------
+https://github.com/bcgsc/abyss
+
+Tools included in this module are
+
+* abyss-fixmate
+* abyss-pe
+]])
+
+local package = "abyss"
+local version = "2.1.5"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
