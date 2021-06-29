6e68df4d2512cfaf7af0290d0b68e9f7d55eb78e
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-29 11:23:11 -0400

    Update test.sh

--- 2.1.5/test.sh
+++ 2.1.5/test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+find . -type f
+
 mkdir test
 cd test
 
 wget http://www.bcgsc.ca/platform/bioinfo/software/abyss/releases/1.3.4/test-data.tar.gz
 tar xzvf test-data.tar.gz
 
-if type "module" > /dev/null; then
-	module load ABySS
-	abyss-pe k=25 name=test \
+singularity exec ../singularity-ABySS-2.1.5.sif abyss-pe k=25 name=test \
 		in='test-data/reads1.fastq test-data/reads2.fastq'
-else
-	singularity exec ../singularity-ABySS-2.1.5.sif abyss-pe k=25 name=test \
-		in='test-data/reads1.fastq test-data/reads2.fastq'
-fi
