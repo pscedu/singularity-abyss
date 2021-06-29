9b9c150665713f4bcee2b6287ebc31dcde20af20
icaoberg <icaoberg@andrew.cmu.edu>
2021-06-29 11:05:36 -0400

    Update test.sh

--- 2.1.5/test.sh
+++ 2.1.5/test.sh
@@ -11,6 +11,6 @@ if type "module" > /dev/null; then
 	abyss-pe k=25 name=test \
 		in='test-data/reads1.fastq test-data/reads2.fastq'
 else
-	singularity exec singularity-ABySS-2.1.5.sif abyss-pe k=25 name=test \
+	singularity exec ../singularity-ABySS-2.1.5.sif abyss-pe k=25 name=test \
 		in='test-data/reads1.fastq test-data/reads2.fastq'
 fi
