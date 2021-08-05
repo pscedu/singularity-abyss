bbd3625b40cef14c8e098f3a78d3f31cbb9e22fc
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-08-05 16:02:18 -0400

    Updated test script

--- 2.1.5/test.sh
+++ 2.1.5/test.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-find . -type f
+set -x
 
-mkdir test
-cd test
-
-wget http://www.bcgsc.ca/platform/bioinfo/software/abyss/releases/1.3.4/test-data.tar.gz
-tar xzvf test-data.tar.gz
-
-singularity exec ../singularity-ABySS-2.1.5.sif abyss-pe k=25 name=test \
-		in='test-data/reads1.fastq test-data/reads2.fastq'
+singularity exec singularity-ABySS-2.1.5.sif abyss-pe --help
+singularity exec singularity-ABySS-2.1.5.sif abyss-fixmate --help
